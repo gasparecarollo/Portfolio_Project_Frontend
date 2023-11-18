bdf815c91a5045e9a6001e6e2e72e609fdfd031a
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-11-18 02:12:10 -0500

    Added Tweet and Tweets

--- src/Components/Tweet.jsx
+++ src/Components/Tweet.jsx
@@ -1,12 +1,36 @@
-function Tweet({ tweet }) {
+import { useState } from "react";
+import TweetEditForm from "./TweetForm";
+
+function Tweet({ tweet, handleDelete, handleSubmit }) {
+    const [viewEditForm, setEditForm] = useState(false);
+    const toggleView = () => {
+        setEditForm(!viewEditForm);
+    };
     return (
         <div className="Tweet">
-            <h4>
-                {tweet.username} <span>{tweet.image_id}</span>
-            </h4>
-            <h5>{tweet.location}</h5>
-            <h5> {tweet.time}</h5>
-            <h5> {tweet.caption}</h5>
+            {tweetEditForm ? (
+                <TweetEditForm tweetDetails={tweet} toggleView={toggleView}
+                    handleSubmit={handleSubmit} />
+            ) : (
+                <div>
+                    <h4>
+                        {tweet.username}
+                        <span> {tweet.image_url}</span>
+                    </h4>
+                    <h5> {tweet.location}</h5>
+                    <h5> {tweet.time}</h5>
+                    <p>{tweet.caption} </p>
+                    <p> {tweet.hashtags}</p>
+                </div>
+            )}
+
+
+            <div className="tweet-actions">
+                <button onClick={toggleView}> {viewEditForm ?
+                    "Cancel" : "Edit this Tweet"}
+                </button>
+                <button onClick={() => handleDelete(tweet.id)}> Delete Tweet</button>
+            </div>
         </div>
     );
 }
--- src/Components/Tweets.jsx
+++ src/Components/Tweets.jsx
@@ -1,12 +1,66 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Tweet from "./Tweet";
+import TweetEditForm from "./TweetForm";
 
 const API = import.meta.env.VITE_BASE_URL;
 
 function Tweets() {
     const [tweets, setTweets] = useState([]);
     let { id } = useParams();
+
+    const handleAdd = (newTweet) => {
+        fetch(`${API}/menu/${id}/tweets`, {
+            method: "POST",
+            body: JSON.stringify(newTweet),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                setTweets([responseJSON, ...tweets]);
+            })
+            .catch((error) => console.error("catch", error));
+    };
+
+    const handleDelete = (id) => {
+        fetch(`${API}/menu/${id}/tweets/${id}`, {
+            method: "DELETE",
+        })
+            .then((response) => {
+                const copyTweetArray = [...tweets];
+                const indexDeletedTweet = copyTweetArray.findIndex((tweet) => {
+                    return tweet.id === id;
+                });
+                copyTweetArray.splice(indexDeletedTweet, 1);
+                setTweets(copyTweetArray);
+            },
+                (error) => console.error(error)
+            )
+            .catch((error) => console.warn("catch", error));
+    };
+
+    const handleEdit = (updatedTweet) => {
+        fetch(`${API}/menu/${id}/tweets/${updatedTweet.id}`, {
+            method: "PUT",
+            body: JSON.stringify(updatedTweet),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                const copyTweetArray = [...tweets];
+                const indexUpdatedTweet = copyTweetArray.findIndex((tweet) => {
+                    return tweet.id === updatedTweet.id;
+                });
+                copyTweetArray[indexUpdatedTweet] = responseJSON;
+                setTweets(copyTweetArray);
+            })
+            .catch((error) => console.error(error));
+    };
+
 
     useEffect(() => {
         fetch(`${API}/menuitems/${id}/tweets`)
